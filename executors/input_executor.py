3e214d5d37a00dea1717968354aa35e734d6a8de
arturo <arturo.chandia@gmail.com>
2026-08-20 16:49:00 -0400

    20082026 16:48

--- executors/input_executor.py
+++ executors/input_executor.py
@@ -17,4 +17,4 @@ class InputExecutor(ActionExecutor):
         print(f"[InputExecutor] Llenando: {action.valor}")
 
         context.driver.fill(result, action.valor)
-        # result.locator.press("Tab")
+        result.locator.press("Tab")
